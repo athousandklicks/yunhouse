2e38fe979e369ed7aef8bd5b62175e9699291835
Ome Itotoh <omescroll@gmail.com>
2017-07-07 06:05:36 +0100

    redesigning first commit

--- resources/views/partials/topbar.blade.php
+++ resources/views/partials/topbar.blade.php
@@ -1,51 +1,52 @@
     <!-- *** TOPBAR ***
- _________________________________________________________ -->
+    _________________________________________________________ -->
     <div id="top">
-        <div class="container">
-            <div class="col-md-6 offer" data-animate="fadeInDown">
-                <a href="#" class="btn btn-success btn-sm" data-animate-hover="shake">Download Yunhouse</a>  <a href="#">Join Palavar Hall and Download Yunhouse for Free</a>
-            </div>
-            <div class="col-md-6" data-animate="fadeInDown">
-                <ul class="menu">
-                    <li><a href="#" data-toggle="modal" data-target="#login-modal">Login</a>
-                    </li>
-                    <li><a href="register.html">Register</a>
-                    </li>
 
-                </ul>
+        <div class="container">
+            <div class="col-md-12" data-animate="fadeInDown">
+                <div class="col-md-3">
+                    <img src="images/static_images/book-logo.png" class="heading-image" alt="">
+                </div>
+            
+
+            <div class="col-md-6">
+                <h1 class="top-heading">yunhouse</h1>
             </div>
         </div>
-        <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="Login" aria-hidden="true">
-            <div class="modal-dialog modal-sm">
-
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                        <h4 class="modal-title" id="Login">Enter Palavar Hall</h4>
-                    </div>
-                    <div class="modal-body">
-                        <form action="customer-orders.html" method="post">
-                            <div class="form-group">
-                                <input type="text" class="form-control" id="email-modal" placeholder="email">
-                            </div>
-                            <div class="form-group">
-                                <input type="password" class="form-control" id="password-modal" placeholder="password">
-                            </div>
-
-                            <p class="text-center">
-                                <button class="btn btn-primary"><i class="fa fa-sign-in"></i> Log in</button>
-                            </p>
-
-                        </form>
-
-                        <p class="text-center text-muted">Not a Member of Palavar Hall?</p>
-                        <p class="text-center text-muted"><a href="register.html"><strong>Become a Member now</strong></a>! It is easy and done in 1&nbsp;minute and gives you access to YUNHOUSE discussion forum on THE AFRICA</p>
-
-                    </div>
-                </div>
-            </div>
         </div>
 
     </div>
+    <div class="modal fade" id="login-modal" tabindex="-1" role="dialog" aria-labelledby="Login" aria-hidden="true">
+        <div class="modal-dialog modal-sm">
+
+            <div class="modal-content">
+                <div class="modal-header">
+                    <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                    <h4 class="modal-title" id="Login">Enter Palavar Hall</h4>
+                </div>
+                <div class="modal-body">
+                    <form action="customer-orders.html" method="post">
+                        <div class="form-group">
+                            <input type="text" class="form-control" id="email-modal" placeholder="email">
+                        </div>
+                        <div class="form-group">
+                            <input type="password" class="form-control" id="password-modal" placeholder="password">
+                        </div>
+
+                        <p class="text-center">
+                            <button class="btn btn-primary"><i class="fa fa-sign-in"></i> Log in</button>
+                        </p>
+
+                    </form>
+
+                    <p class="text-center text-muted">Not a Member of Palavar Hall?</p>
+                    <p class="text-center text-muted"><a href="register.html"><strong>Become a Member now</strong></a>! It is easy and done in 1&nbsp;minute and gives you access to YUNHOUSE discussion forum on THE AFRICA</p>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div>
 
     <!-- *** TOP BAR END *** -->
